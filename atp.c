f39b7c1ce9243d7227a476d99a7b5193fd8019af
Rohit Grover <rgrover1@gmail.com>
2014-02-16 18:17:47 +0100

    filled out compute_smoothening_scale_ratio()

--- atp.c
+++ atp.c
@@ -1387,75 +1387,74 @@ atp_compute_smoothening_scale_ratio(atp_stroke_t *strokep, int *numerator,
 {
 	*numerator = 0, *denominator = 0; /* default values. */
 
-	// // u_int vel_squared; /* Square of the velocity vector's magnitude. */
-	// // u_int vel_squared_smooth;
+	int dxdt;
+	int dydt;
+	dxdt = strokep->instantaneous_dx;
+	dydt = strokep->instantaneous_dy;
 
-	// /* Table holding (10 * sqrt(x)) for x between 1 and 256. */
-	// static uint8_t sqrt_table[256] = {
-	// 	10, 14, 17, 20, 22, 24, 26, 28,
-	// 	30, 31, 33, 34, 36, 37, 38, 40,
-	// 	41, 42, 43, 44, 45, 46, 47, 48,
-	// 	50, 50, 51, 52, 53, 54, 55, 56,
-	// 	57, 58, 59, 60, 60, 61, 62, 63,
-	// 	64, 64, 65, 66, 67, 67, 68, 69,
-	// 	70, 70, 71, 72, 72, 73, 74, 74,
-	// 	75, 76, 76, 77, 78, 78, 79, 80,
-	// 	80, 81, 81, 82, 83, 83, 84, 84,
-	// 	85, 86, 86, 87, 87, 88, 88, 89,
-	// 	90, 90, 91, 91, 92, 92, 93, 93,
-	// 	94, 94, 95, 95, 96, 96, 97, 97,
-	// 	98, 98, 99, 100, 100, 100, 101, 101,
-	// 	102, 102, 103, 103, 104, 104, 105, 105,
-	// 	106, 106, 107, 107, 108, 108, 109, 109,
-	// 	110, 110, 110, 111, 111, 112, 112, 113,
-	// 	113, 114, 114, 114, 115, 115, 116, 116,
-	// 	117, 117, 117, 118, 118, 119, 119, 120,
-	// 	120, 120, 121, 121, 122, 122, 122, 123,
-	// 	123, 124, 124, 124, 125, 125, 126, 126,
-	// 	126, 127, 127, 128, 128, 128, 129, 129,
-	// 	130, 130, 130, 131, 131, 131, 132, 132,
-	// 	133, 133, 133, 134, 134, 134, 135, 135,
-	// 	136, 136, 136, 137, 137, 137, 138, 138,
-	// 	138, 139, 139, 140, 140, 140, 141, 141,
-	// 	141, 142, 142, 142, 143, 143, 143, 144,
-	// 	144, 144, 145, 145, 145, 146, 146, 146,
-	// 	147, 147, 147, 148, 148, 148, 149, 149,
-	// 	150, 150, 150, 150, 151, 151, 151, 152,
-	// 	152, 152, 153, 153, 153, 154, 154, 154,
-	// 	155, 155, 155, 156, 156, 156, 157, 157,
-	// 	157, 158, 158, 158, 159, 159, 159, 160
-	// };
-	// const u_int N = sizeof(sqrt_table) / sizeof(sqrt_table[0]);
+	/* Compute a smoothened magnitude_squared of the stroke's velocity. */
+	u_int vel_squared; /* Square of the velocity vector's magnitude. */
+	u_int vel_squared_smooth;
+	vel_squared = dxdt * dxdt + dydt * dydt;
+	vel_squared_smooth = (3 * strokep->velocity_squared + vel_squared) >> 2;
+	strokep->velocity_squared = vel_squared_smooth; /* retained as history */
+	if ((vel_squared == 0) || (vel_squared_smooth == 0))
+		return; /* returning (numerator == 0) will imply zero movement*/
 
-	// int dxdt;
-	// int dydt;
-	// dxdt = strokep->instantaneous_dx;
-	// dydt = strokep->instantaneous_dy;
+	/* Table holding (10 * sqrt(x)) for x between 1 and 256. */
+	static uint8_t sqrt_table[256] = {
+		10, 14, 17, 20, 22, 24, 26, 28,
+		30, 31, 33, 34, 36, 37, 38, 40,
+		41, 42, 43, 44, 45, 46, 47, 48,
+		50, 50, 51, 52, 53, 54, 55, 56,
+		57, 58, 59, 60, 60, 61, 62, 63,
+		64, 64, 65, 66, 67, 67, 68, 69,
+		70, 70, 71, 72, 72, 73, 74, 74,
+		75, 76, 76, 77, 78, 78, 79, 80,
+		80, 81, 81, 82, 83, 83, 84, 84,
+		85, 86, 86, 87, 87, 88, 88, 89,
+		90, 90, 91, 91, 92, 92, 93, 93,
+		94, 94, 95, 95, 96, 96, 97, 97,
+		98, 98, 99, 100, 100, 100, 101, 101,
+		102, 102, 103, 103, 104, 104, 105, 105,
+		106, 106, 107, 107, 108, 108, 109, 109,
+		110, 110, 110, 111, 111, 112, 112, 113,
+		113, 114, 114, 114, 115, 115, 116, 116,
+		117, 117, 117, 118, 118, 119, 119, 120,
+		120, 120, 121, 121, 122, 122, 122, 123,
+		123, 124, 124, 124, 125, 125, 126, 126,
+		126, 127, 127, 128, 128, 128, 129, 129,
+		130, 130, 130, 131, 131, 131, 132, 132,
+		133, 133, 133, 134, 134, 134, 135, 135,
+		136, 136, 136, 137, 137, 137, 138, 138,
+		138, 139, 139, 140, 140, 140, 141, 141,
+		141, 142, 142, 142, 143, 143, 143, 144,
+		144, 144, 145, 145, 145, 146, 146, 146,
+		147, 147, 147, 148, 148, 148, 149, 149,
+		150, 150, 150, 150, 151, 151, 151, 152,
+		152, 152, 153, 153, 153, 154, 154, 154,
+		155, 155, 155, 156, 156, 156, 157, 157,
+		157, 158, 158, 158, 159, 159, 159, 160
+	};
+	const u_int N = sizeof(sqrt_table) / sizeof(sqrt_table[0]);
 
-	// /* Compute a smoothened magnitude_squared of the stroke's velocity. */
-	// vel_squared = dxdt * dxdt + dydt * dydt;
-	// vel_squared_smooth = (3 * strokep->velocity_squared + vel_squared) >> 2;
-	// strokep->velocity_squared = vel_squared_smooth; /* retained as history */
-	// if ((vel_squared == 0) || (vel_squared_smooth == 0))
-	// 	return; /* returning (numerator == 0) will imply zero movement*/
+	/*
+	 * In order to determine the overall movement scale factor,
+	 * we're actually interested in the effect of smoothening upon
+	 * the *magnitude* of velocity; i.e. we need to compute the
+	 * square-root of (vel_squared_smooth / vel_squared) in the
+	 * form of a numerator and denominator.
+	 */
 
-	// /*
-	//  * In order to determine the overall movement scale factor,
-	//  * we're actually interested in the effect of smoothening upon
-	//  * the *magnitude* of velocity; i.e. we need to compute the
-	//  * square-root of (vel_squared_smooth / vel_squared) in the
-	//  * form of a numerator and denominator.
-	//  */
+	/* Keep within the bounds of the square-root table. */
+	while ((vel_squared > N) || (vel_squared_smooth > N)) {
+		/* Dividing uniformly by 2 won't disturb the final ratio. */
+		vel_squared        >>= 1;
+		vel_squared_smooth >>= 1;
+	}
 
-	// /* Keep within the bounds of the square-root table. */
-	// while ((vel_squared > N) || (vel_squared_smooth > N)) {
-	// 	/* Dividing uniformly by 2 won't disturb the final ratio. */
-	// 	vel_squared        >>= 1;
-	// 	vel_squared_smooth >>= 1;
-	// }
-
-	// *numerator   = sqrt_table[vel_squared_smooth - 1];
-	// *denominator = sqrt_table[vel_squared - 1];
+	*numerator   = sqrt_table[vel_squared_smooth - 1];
+	*denominator = sqrt_table[vel_squared - 1];
 }
 
 /*
