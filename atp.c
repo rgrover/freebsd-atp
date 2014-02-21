b79c3b472c99bc297ed1853db4b2e4e6ee725b50
Rohit Grover <rgrover1@gmail.com>
2014-02-21 16:08:55 +0100

    white space diff

--- atp.c
+++ atp.c
@@ -1012,8 +1012,8 @@ atp_softc_unpopulate(struct atp_softc *sc)
 void
 fg_interpret_sensor_data(struct atp_softc *sc, unsigned data_len)
 {
-#define FG_MAX_XSENSORS    26
-#define FG_MAX_YSENSORS    16
+#define FG_MAX_XSENSORS 26
+#define FG_MAX_YSENSORS 16
 	static int cur_x[FG_MAX_XSENSORS]; /* current sensor readings */
 	static int cur_y[FG_MAX_YSENSORS];
 	static int base_x[FG_MAX_XSENSORS];
